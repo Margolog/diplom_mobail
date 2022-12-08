4f9a12bab8ce7e41e91999486d2f26b250905df6
Logunova Margarita <logunovar@gmail.com>
2022-12-08 21:50:09 +0300

    new

---
+++ test/android_test.py
@@ -0,0 +1,15 @@
+import allure
+from appium.webdriver.common.appiumby import AppiumBy
+from selene import have
+from selene.support.conditions import be
+from selene.support.shared import browser
+
+
+def test_search_browserstack():
+    with allure.step('Выполняем поиск'):
+        browser.element((AppiumBy.ACCESSIBILITY_ID, "Search Wikipedia")).click()
+        browser.element((AppiumBy.ID, "org.wikipedia.alpha:id/search_src_text")).type("Turkey")
+    with allure.step('Выполняем проверку результатов поиска'):
+        browser.all((AppiumBy.CLASS_NAME, "android.widget.TextView")).should(have.size_greater_than(0))
+
+
