910375ff97bc2862c328d3c19d5ac452849d962f
Logunova Margarita <logunovar@gmail.com>
2022-12-11 21:34:45 +0300

    new

--- test/android_test.py
+++ test/android_test.py
@@ -1,11 +1,17 @@
 import allure
+import pytest
+from _pytest.fixtures import FixtureRequest
 from appium.webdriver.common.appiumby import AppiumBy
+from pydantic import schema
 from selene import have
 from selene.support.shared import browser
+import utils.by
+from utils import by
+import conftest
 
 
-
-def test_search_news():
+def test_search_news(request: FixtureRequest):
+    allure.dynamic.title(" ".join(request.node.name.split("_")[1:]).capitalize())
     with allure.step('Выполняем поиск'):
         browser.element((AppiumBy.ACCESSIBILITY_ID, "Search Wikipedia")).click()
         browser.element((AppiumBy.ID, "org.wikipedia.alpha:id/search_src_text")).type("Turkey")
