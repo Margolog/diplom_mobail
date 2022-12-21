5ac80418714c53dc33c4801c16a2490679d4c846
Logunova Margarita <logunovar@gmail.com>
2022-12-21 13:29:25 +0300

    new

--- conftest.py
+++ conftest.py
@@ -4,35 +4,7 @@ from appium import webdriver
 from dotenv import load_dotenv
 from utils.attachments import *
 from selene.support.shared import browser
-from appium.options.android import UiAutomator2Options
 
-
-
-# @pytest.fixture(scope='function', autouse=True)
-# def setup_browser():
-#     load_dotenv()
-#     username = os.getenv('USER_NAME')
-#     accesskey = os.getenv('ACCESS_KEY')
-#     app = os.getenv('APP')
-#
-#     options = UiAutomator2Options().load_capabilities({
-#         "platformName": "android",
-#         "platformVersion": "9.0",
-#         "deviceName": "Google Pixel 3",
-#         "app": f'{app}',
-#         'bstack:options': {
-#             "projectName": "First Python project",
-#             "buildName": "browserstack-build-1",
-#             "sessionName": "BStack first_test",
-#             "userName": f'{username}',
-#             "accessKey": f'{accesskey}'
-#         }
-#     })
-#
-#     browser.config.driver = webdriver.Remote(
-#         command_executor="http://hub.browserstack.com/wd/hub",
-#         options=options,
-#     )
 
 @pytest.fixture(scope='function', autouse=True)
 def setup_browser():
