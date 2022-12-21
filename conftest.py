74c48b772fa471826fe9b05766cdbc7fb3e0f455
Logunova Margarita <logunovar@gmail.com>
2022-12-21 13:04:17 +0300

    new

--- conftest.py
+++ conftest.py
@@ -4,31 +4,59 @@ from appium import webdriver
 from dotenv import load_dotenv
 from utils.attachments import *
 from selene.support.shared import browser
+from appium.options.android import UiAutomator2Options
 
 
 @pytest.fixture(scope='function', autouse=True)
 def setup_browser():
     load_dotenv()
-    options = {
+    username = os.getenv('USER_NAME')
+    accesskey = os.getenv('ACCESS_KEY')
+    app = os.getenv('APP')
+
+    options = UiAutomator2Options().load_capabilities({
         "platformName": "android",
         "platformVersion": "9.0",
         "deviceName": "Google Pixel 3",
-        "app": "bs://c700ce60cf13ae8ed97705a55b8e022f13c5827c",
+        "app": app,
         'bstack:options': {
             "projectName": "First Python project",
-            "buildName": "browserstack-build-DEMO",
-            "sessionName": "BStack first_test"
+            "buildName": "browserstack-build-1",
+            "sessionName": "BStack first_test",
+            "userName": username,
+            "accessKey": accesskey
         }
-    }
-    USER_NAME = os.getenv('USER_NAME')
-    ACCESS_KEY = os.getenv('ACCESS_KEY')
-
-    browser.config.driver = webdriver.Remote(
-        command_executor=f"https://{USER_NAME}:{ACCESS_KEY}@hub-cloud.browserstack.com/wd/hub",
-        options=options)
+    })
+    browser.config.driver = webdriver.Remote('http://hub.browserstack.com/wd/hub', options=options)
 
     browser.config.timeout = 4
     yield setup_browser
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     add_video(browser)
     add_screenshot(browser)
