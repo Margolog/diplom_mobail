a83a43b0e9f75d9e8414868360d8b8ac119646a9
Logunova Margarita <logunovar@gmail.com>
2022-12-21 12:04:05 +0300

    new

--- test/android_test.py
+++ test/android_test.py
@@ -11,6 +11,16 @@ def test_search_symbols():
     app.search_results_page.assert_results_exist()
 
 
+def test_search_space():
+    app.main_page.search_item('  ')
+    app.search_results_page.assert_results_exist()
+
+
+def test_search_numbers():
+    app.main_page.search_item('11111')
+    app.search_results_page.assert_results_exist()
+
+
 def test_return_to_main():
     app.main_page.search_item('Turkey')
     app.search_results_page.click_on_return()
