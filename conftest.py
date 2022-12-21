56f8812646f4e7e6778b37afbba1dd0352dd6127
Logunova Margarita <logunovar@gmail.com>
2022-12-21 13:14:41 +0300

    new

--- conftest.py
+++ conftest.py
@@ -18,24 +18,28 @@ def setup_browser():
         "platformName": "android",
         "platformVersion": "9.0",
         "deviceName": "Google Pixel 3",
-        "app": app,
+        "app": f'{app}',
         'bstack:options': {
             "projectName": "First Python project",
             "buildName": "browserstack-build-1",
             "sessionName": "BStack first_test",
-            "userName": username,
-            "accessKey": accesskey
+            "userName": f'{username}',
+            "accessKey": f'{accesskey}'
         }
     })
-    browser.config.driver = webdriver.Remote('http://hub.browserstack.com/wd/hub', options=options)
+
+    browser.config.driver = webdriver.Remote(
+        command_executor="http://hub.browserstack.com/wd/hub",
+        options=options,
+    )
 
     browser.config.timeout = 4
     yield setup_browser
 
-
-
-
-
+    add_video(browser)
+    add_screenshot(browser)
+    add_xml_dump(browser)
+    browser.quit()
 
 
 
