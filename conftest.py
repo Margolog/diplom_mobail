913bab995b5261e13da54774c04064b5258e7737
Logunova Margarita <logunovar@gmail.com>
2022-12-21 13:20:50 +0300

    new

--- conftest.py
+++ conftest.py
@@ -7,30 +7,52 @@ from selene.support.shared import browser
 from appium.options.android import UiAutomator2Options
 
 
+
+# @pytest.fixture(scope='function', autouse=True)
+# def setup_browser():
+#     load_dotenv()
+#     username = os.getenv('USER_NAME')
+#     accesskey = os.getenv('ACCESS_KEY')
+#     app = os.getenv('APP')
+#
+#     options = UiAutomator2Options().load_capabilities({
+#         "platformName": "android",
+#         "platformVersion": "9.0",
+#         "deviceName": "Google Pixel 3",
+#         "app": f'{app}',
+#         'bstack:options': {
+#             "projectName": "First Python project",
+#             "buildName": "browserstack-build-1",
+#             "sessionName": "BStack first_test",
+#             "userName": f'{username}',
+#             "accessKey": f'{accesskey}'
+#         }
+#     })
+#
+#     browser.config.driver = webdriver.Remote(
+#         command_executor="http://hub.browserstack.com/wd/hub",
+#         options=options,
+#     )
+
 @pytest.fixture(scope='function', autouse=True)
 def setup_browser():
     load_dotenv()
-    username = os.getenv('USER_NAME')
-    accesskey = os.getenv('ACCESS_KEY')
-    app = os.getenv('APP')
-
-    options = UiAutomator2Options().load_capabilities({
+    options = {
         "platformName": "android",
         "platformVersion": "9.0",
         "deviceName": "Google Pixel 3",
-        "app": f'{app}',
+        "app": "bs://c700ce60cf13ae8ed97705a55b8e022f13c5827c",
         'bstack:options': {
             "projectName": "First Python project",
-            "buildName": "browserstack-build-1",
-            "sessionName": "BStack first_test",
-            "userName": f'{username}',
-            "accessKey": f'{accesskey}'
+            "buildName": "browserstack-build-DEMO",
+            "sessionName": "BStack first_test"
         }
-    })
-
+    }
+    USER_NAME = os.getenv('USER_NAME')
+    ACCESS_KEY = os.getenv('ACCESS_KEY')
     browser.config.driver = webdriver.Remote(
-        command_executor="http://hub.browserstack.com/wd/hub",
-        options=options,
+        command_executor=f"https://{USER_NAME}:{ACCESS_KEY}@hub-cloud.browserstack.com/wd/hub",
+        desired_capabilities=options
     )
 
     browser.config.timeout = 4
